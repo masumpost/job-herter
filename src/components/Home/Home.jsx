632d0eb4234a71fc4491dc8aecfc6fd463a6e6ba
md-masum <masum.nextu@gmail.com>
2023-04-10 00:26:02 +0600

    nav bar with logo and button

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Home = () => {
+    return (
+        <div className='md:mx-4'>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Home;
